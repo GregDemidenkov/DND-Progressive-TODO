d8b75b41ea167dac3a9001f6f97d6c7a64bc8e1e
GregDemidenkov <greg.demidenkov@yandex.ru>
2023-04-26 00:46:33 +0300

    Refactor styles, add simple adaptive

--- client/src/components/common/Header/Header.tsx
+++ client/src/components/common/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header: FC = () => {
   
   return (
     <header className = {styles.header}>
-      <h1>DND-TODO</h1>
+      <h1>DND-TODO<span>Progressive</span></h1>
     </header>
   )
 }
